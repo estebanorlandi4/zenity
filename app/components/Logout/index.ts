edaf58004d4dce15846cf28f0577d4c69546d0cd
estebanorlandi4 <estebanorlandi4@gmail.com>
2022-07-11 18:22:21 -0300

    feat: working with logout redirect

---
+++ app/components/Logout/index.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  font-size: calc(1rem + 2vw);
+
+  .text {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+
+    p {
+      font-size: 1rem;
+    }
+    .countdown {
+    }
+  }
+
+  .redirect {
+    padding: 0.75rem 1rem;
+    font-weight: 700;
+  }
+`;
--- app/pages/auth/logout/index.tsx
+++ app/pages/auth/logout/index.tsx
@@ -1,14 +1,39 @@
+import { Container } from 'components/Logout';
 import { signOut, useSession } from 'next-auth/react';
-import { useEffect } from 'react';
+import Router from 'next/router';
+import { useCallback, useEffect, useState } from 'react';
+
+const SECONDS = 15;
 
 function LogOut() {
   const { data: session } = useSession();
+  const [time, setTime] = useState(SECONDS);
 
-  useEffect(() => {
-    signOut();
+  const redirect = useCallback(() => {
+    Router.push('/');
   }, []);
 
-  return <div>LogOut</div>;
+  useEffect(() => {
+    if (session) signOut();
+    else setTimeout(redirect, SECONDS * 1000);
+  }, [session, redirect]);
+
+  useEffect(() => {
+    if (time === 0) setTime(SECONDS);
+    const interval = setInterval(() => setTime(time - 1), 1000);
+    return () => clearInterval(interval);
+  }, [time]);
+
+  return (
+    <Container>
+      <div className="text">
+        <p>Redirection in</p> <span className="countdown">{time}</span>
+      </div>
+      <button className="redirect" onClick={redirect}>
+        Redirect
+      </button>
+    </Container>
+  );
 }
 
 export default LogOut;
